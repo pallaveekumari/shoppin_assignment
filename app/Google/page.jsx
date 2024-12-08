7825f45f935108a975b99553904f571e2a93a256
pallaveekumari <pallaveeratnakar1714@gmail.com>
2024-12-08 19:19:13 +0530

    google-logs-search-bar-implemented

--- app/Google/page.jsx
+++ app/Google/page.jsx
@@ -4,7 +4,6 @@ import styles from "./Google.module.css";
 const page = () => {
   return (
     <div className={styles.page}>
-      {/* Navbar Starts */}
       <div className={styles.navbar}>
         <div className={styles.leftBox}>
           <a
@@ -39,6 +38,55 @@ const page = () => {
             alt=""
           />
           <div className={styles.profile}>P</div>
+        </div>
+      </div>
+      <div className={styles.contentBox}>
+        <img
+          className={styles.googleLogo}
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div className={styles.inputContainer}>
+          <div className={styles.inputLogosBox}>
+            <img
+              className={styles.inputLogos}
+              src="https://static-00.iconduck.com/assets.00/search-icon-2048x2048-cmujl7en.png"
+              alt=""
+            />
+          </div>
+          <input className={styles.textField} type="text" />
+          <div className={styles.inputLogosBox}>
+            <img
+              style={{ opacity: 1 }}
+              className={styles.miclogo}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Google_mic.svg/1432px-Google_mic.svg.png"
+              alt=""
+            />
+          </div>
+          <div className={styles.inputLogosBox}>
+            <img
+              style={{ opacity: 1 }}
+              className={styles.lenslogo}
+              src="https://seeklogo.com/images/G/google-lens-logo-0F69C74B83-seeklogo.com.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className={styles.buttonsBox}>
+          <button>Google Search</button>
+          <button>I'm Feeling Lucky</button>
+        </div>
+        <div className={styles.languageBox}>
+          <span>Google offered in:</span>
+          <a href="https://www.google.com/">हिन्दी</a>
+          <a href="https://www.google.com/">বাংলা</a>
+          <a href="https://www.google.com/">తెలుగు</a>
+          <a href="https://www.google.com/">मराठी</a>
+          <a href="https://www.google.com/">தமிழ்</a>
+          <a href="https://www.google.com/">ગુજરાતી</a>
+          <a href="https://www.google.com/">ಕನ್ನಡ</a>
+          <a href="https://www.google.com/">മലയാളം</a>
+          <a href="https://www.google.com/">ਪੰਜਾਬੀ</a>
         </div>
       </div>
     </div>
